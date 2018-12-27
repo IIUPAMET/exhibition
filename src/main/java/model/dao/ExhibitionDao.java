e35b252b89c47a6e155d8a5fe34a540cc7800883
Mykola <filimonovkolya97@gmail.com>
2018-12-27 17:48:52 +0200

    Ticket logic

--- src/main/java/model/dao/ExhibitionDao.java
+++ src/main/java/model/dao/ExhibitionDao.java
@@ -3,8 +3,10 @@ package model.dao;
 import model.entity.Exhibition;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ExhibitionDao extends GenericDao<Exhibition> {
-    public void crateExhibition(String name, LocalDate start, LocalDate end, String theme, String author, String description);
-    public void exhibitionWithTickets(Exhibition exhibition, Integer numOfTickets);
+    void crateExhibition(String name, LocalDate start, LocalDate end, String theme, String author, String description);
+    void exhibitionWithTickets(Exhibition exhibition, Integer numOfTickets);
+    List<Exhibition> getExhibitionForUser(Integer userId);
 }
